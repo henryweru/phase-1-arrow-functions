4de21da15589a64c8daa4e4a85450df62fc652d7
henry <henry.waweru@student.moringaschool.com>
2023-03-20 13:23:57 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  // arrow function called square
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // arrow function called add
+  const add = (num1, num2) => {
+    return num1 + num2;
+  }
+  
